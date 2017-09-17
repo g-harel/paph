57a6e35add2cc74d0eaeec5cc496fd9d9751847f
g-harel <gabrielj.harel@gmail.com>
2017-09-16 23:29:01 -0400

    added test helper for clearer tests

--- test.js
+++ test.js
@@ -1,40 +1,44 @@
 const paph = require('./index');
 
+const add = (store, start, end, weight = 1) => {
+    store.add(start, end, weight, (a) => a += ` ${start}${end}`);
+};
+
 test('finds a path when it exists', () => {
     const p = paph();
-    p.add('0', '1', 1, (a) => a += ' 01');
-    p.add('1', '2', 1, (a) => a += ' 12');
-    p.add('2', '3', 1, (a) => a += ' 23');
+    add(p, '0', '1');
+    add(p, '1', '2');
+    add(p, '2', '3');
     expect(p.query('0', '3')('')).toBe(' 01 12 23');
     expect(p.query('0', '2')('')).toBe(' 01 12');
 });
 
 test('gracefully fails when no path is found', () => {
     const p = paph();
-    p.add('0', '1', 1, (a) => a += ' 01');
-    p.add('1', '2', 1, (a) => a += ' 12');
-    p.add('2', '3', 1, (a) => a += ' 23');
+    add(p, '0', '1');
+    add(p, '1', '2');
+    add(p, '2', '3');
     expect(() => p.query('1', '0')('')).toThrow(/1[^]*0/);
     expect(() => p.query('a', 'b')('')).toThrow(/a[^]*b/);
 });
 
 test('finds the shortest path, regardless of order', () => {
     const p = paph();
-    p.add('0', '2', 1, (a) => a += ' 02');
-    p.add('0', '1', 1, (a) => a += ' 01');
-    p.add('1', '2', 1, (a) => a += ' 12');
-    p.add('2', '3', 1, (a) => a += ' 23');
+    add(p, '0', '1');
+    add(p, '1', '2');
+    add(p, '0', '2');
+    add(p, '2', '3');
     expect(p.query('0', '3')('')).toBe(' 02 23');
-    p.add('0', '3', 1, (a) => a += ' 03');
+    add(p, '0', '3');
     expect(p.query('0', '3')('')).toBe(' 03');
 });
 
 test('finds the path with the lowest weight', () => {
     const p = paph();
-    p.add('0', '1', 2, (a) => a += ' 01');
-    p.add('1', '2', 2, (a) => a += ' 12');
-    p.add('0', '2', 5, (a) => a += ' 02');
-    p.add('2', '3', 1, (a) => a += ' 23');
+    add(p, '0', '1', 2);
+    add(p, '1', '2', 2);
+    add(p, '0', '2', 5);
+    add(p, '2', '3', 1);
     expect(p.query('0', '3')('')).toBe(' 01 12 23');
 });
 
@@ -45,28 +49,28 @@ test('doesn\'t allow negative weights', () => {
 
 test('doesn\'t get stuck in cycles', () => {
     const p = paph();
-    p.add('0', '1', 1, (a) => a += ' 01');
-    p.add('1', '0', 1, (a) => a += ' 10');
+    add(p, '0', '1');
+    add(p, '1', '0');
     expect(() => p.query('0', 'a')('')).toThrow(Error);
-    p.add('2', '2', 1, (a) => a += ' 22');
+    add(p, '2', '2');
     expect(() => p.query('2', 'a')('')).toThrow(Error);
 });
 
 test('can freeze the current state', () => {
     const p = paph();
-    p.add('0', '1', 1, (a) => a += ' 01');
-    p.add('1', '2', 1, (a) => a += ' 12');
+    add(p, '0', '1');
+    add(p, '1', '2');
     expect(p.query('0', '2')('')).toBe(' 01 12');
     expect(p.freeze().query('0', '2')('')).toBe(' 01 12');
 });
 
 test('frozen state cannot be modified', () => {
     const p = paph();
-    p.add('0', '1', 1, (a) => a += ' 01');
-    p.add('1', '2', 1, (a) => a += ' 12');
+    add(p, '0', '1');
+    add(p, '1', '2');
     expect(p.query('0', '2')('')).toBe(' 01 12');
     const q = p.freeze();
-    p.add('0', '2', 1, (a) => a += ' 02');
+    add(p, '0', '2');
     expect(p.query('0', '2')('')).toBe(' 02');
     expect(q.query('0', '2')('')).toBe(' 01 12');
 });
@@ -76,9 +80,9 @@ test('freezing should increase performance', () => {
     const cap = 1000;
     const queries = 50;
     for(let i = 0; i <= cap; ++i) {
-        let final = String(Math.min(i+1+Math.floor(Math.random()*4), cap));
+        let final = Math.min(i+1+Math.floor(Math.random()*4), cap);
         let weight = 1+Math.floor(Math.random()*4);
-        p.add(String(i), final, weight, () => {});
+        p.add(String(i), String(final), weight, () => {});
     }
     const timeRegularStart = process.hrtime();
     for (let i = 0; i < queries; ++i) {
@@ -91,28 +95,28 @@ test('freezing should increase performance', () => {
         q.query('0', String(cap));
     }
     const timeFrozen = process.hrtime(timeFrozenStart)[0] * 1000 + process.hrtime(timeFrozenStart)[1] / 1000000;
-    console.log(`frozen was ${(timeRegular/timeFrozen).toFixed(5)}x faster`);
+    console.log(`frozen was ${(timeRegular/timeFrozen).toFixed(5)}x faster (${timeRegular}ms vs. ${timeFrozen}ms)`);
     expect(timeFrozen < timeRegular).toBe(true);
 });
 
 test('can fork the current state', () => {
     const p = paph();
-    p.add('0', '1', 1, (a) => a += ' 01');
-    p.add('1', '2', 1, (a) => a += ' 12');
+    add(p, '0', '1');
+    add(p, '1', '2');
     expect(p.query('0', '2')('')).toBe(' 01 12');
     expect(p.fork().query('0', '2')('')).toBe(' 01 12');
 });
 
 test('forked state is independant of original', () => {
     const p = paph();
-    p.add('0', '1', 1, (a) => a += ' 01');
-    p.add('1', '2', 1, (a) => a += ' 12');
+    add(p, '0', '1');
+    add(p, '1', '2');
     expect(p.query('0', '2')('')).toBe(' 01 12');
     const q = p.fork();
-    p.add('0', '2', 1, (a) => a += ' 02');
+    add(p, '0', '2');
     expect(p.query('0', '2')('')).toBe(' 02');
     expect(q.query('0', '2')('')).toBe(' 01 12');
-    q.add('0', '3', 1, (a) => a += ' 03');
+    add(q, '0', '3');
     expect(() => p.query('0', '3')).toThrow(Error);
     expect(q.query('0', '3')('')).toBe(' 03');
 });
